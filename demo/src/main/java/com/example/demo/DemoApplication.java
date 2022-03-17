5efe5460fbdbd99f39caa9a7707a0b133f13d698
kajmanf <filip.kajanovic@gmail.com>
2022-03-17 11:59:14 +0100

    Get probability of one figth

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,8 @@ import com.example.demo.model.Duelist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -16,20 +15,81 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("Welcome to Winning probability app!");
 
-        int duelistCounter = 0;
+        ////
+        System.out.println("Are you ready to start? (y/n):");
+        Scanner inputAppStart = new Scanner(System.in);
+        String appStarter = inputAppStart.nextLine();
+        boolean continuer = Methods.yesOrNoChecker(appStarter);
+        ////
 
+        /*
         List<Duelist> duelists = Methods.createListOfDuelist();
+        */
+        ///////////
+        List<Duelist> duelists = new ArrayList<>();
+        duelists.add(new Duelist("Adam", 0.556456));
+        duelists.add(new Duelist("Eva", 0.1));
+        duelists.add(new Duelist("Jirka", 0.45121455));
+        ///////////
+
 
         System.out.println("Your list of duelists:");
+        int duelistPosition = 1;
         for (Duelist duelist : duelists) {
-            System.out.println(duelist.getName() + " with " + duelist.getAccuracy() + " accuracy.");
+            System.out.println(duelistPosition +". " + duelist.getName() + " with " + duelist.getAccuracy()*100 + "% accuracy.");
+            duelistPosition++;
         }
+
+        List<Duelist> duelistsSorted = duelists.stream()
+                .sorted(Comparator.comparing(Duelist::getAccuracy))
+                .collect(Collectors.toList());
+
+        System.out.println("Choose duelist you wanna test (by position number of your list):");
+        int userToTest = Methods.chooseDuelist(duelistsSorted.size());
+        System.out.println("You have chosen " + duelists.get(userToTest-1).getName());
+
+        System.out.println("Choose number of tests you wanna make for each availible option:");
+        int numberOfTests = Methods.numberOfTests();
+        System.out.println("We are goonna make " + numberOfTests + " tests for each of " + (duelistsSorted.size()-1) + " options.");
+
+        System.out.println(".........................................................................................");
+        System.out.println("May we procede to start testing? (y/n):");
+        Scanner inputTestStarter = new Scanner(System.in);
+        String startTests = inputTestStarter.nextLine();
+        boolean continuer2 = Methods.yesOrNoChecker(startTests);
+
+        String duelOrder = "Duelists are gonna duel in this order: ";
+        for (Duelist duelist : duelists) {
+            duelOrder = duelOrder + duelist.getName() + " > ";
+        }
+        String duelOrder2 = duelOrder.substring(0,duelOrder.length()-2);
+        System.out.println(duelOrder2);
+
+        Methods.pressEnterToContinue();
+
+        for (int i = 0; i < duelists.size(); i++) {
+            
+        }
+
+        int[] results = Methods.battle(duelistsSorted.get(0), duelistsSorted.get(1), numberOfTests);
+        System.out.println("Results are: " + results[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         System.out.println("xxxxx");
-        /*System.out.println("Please type your name:");
-        Scanner input = new Scanner(System.in);
-        String username = input.nextLine();
-        System.out.println("Hello " + username + "!");*/
     }
 }
--- demo/src/main/java/com/example/demo/methods/Methods.java
+++ demo/src/main/java/com/example/demo/methods/Methods.java
@@ -1,10 +1,9 @@
 package com.example.demo.methods;
 
 import com.example.demo.model.Duelist;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 public class Methods {
@@ -56,7 +55,7 @@ public class Methods {
                         next1 = true;
                     }
                 } catch (NumberFormatException nfe){
-                    System.out.println("Not valid input, try again! (should look like 0.1234567)");
+                    System.out.println("Not valid input, try again! (should look like '0.1234567')");
                 }
             }
 
@@ -65,30 +64,95 @@ public class Methods {
 
             if (duelistCounter > 1){
                 System.out.println("You currently have " + duelistCounter + " duelists, do you wanna add some more? (y/n):");
-                boolean next2 = false;
-                while (next2 == false) {
-                    Scanner inputAddMore = new Scanner(System.in);
-                    String addMore = inputAddMore.nextLine();
-                    if (addMore.equals("n")) {
-                        finish = true;
-                        next2 = true;
-                    } else if (addMore.equals("y")){
-                        next2 = true;
-                    } else {
-                        System.out.println("Not valid input, try again! (You should use character 'y' or 'n')");
-                    }
+                Scanner inputAddMore = new Scanner(System.in);
+                String addMore = inputAddMore.nextLine();
+                boolean continuer = yesOrNoChecker(addMore);
+                if (!continuer){
+                    finish = true;
                 }
             }
             duelistCounter++;
         }
 
-        List<Duelist> duelistsSorted = duelists.stream()
-                .sorted(Comparator.comparing(Duelist::getAccuracy))
-                .collect(Collectors.toList());
-
-        return duelistsSorted;
+        return duelists;
     }
 
+    public static boolean yesOrNoChecker(String stringToCheck) {
+        boolean answer = true;
+        boolean next = false;
+        String continuer = stringToCheck;
+        while (next == false) {
+            if (continuer.equals("n")) {
+                answer = false;
+                next = true;
+            } else if (continuer.equals("y")) {
+                next = true;
+            } else {
+                System.out.println("Not valid input, try again! (You should use character 'y' or 'n')");
+                Scanner inputContinuer = new Scanner(System.in);
+                continuer = inputContinuer.nextLine();
+            }
+        }
+        return answer;
+    }
 
+    public static Integer chooseDuelist(int nmbrOfUsers) {
+        boolean next = false;
+        int userPosition = 0;
+        while (next == false) {
+            Scanner inputContinuer = new Scanner(System.in);
+            String continuer = inputContinuer.nextLine();
+            try {
+                int integer = Integer.parseInt(continuer);
+                if (integer > 0 && integer <= nmbrOfUsers){
+                    userPosition = integer;
+                    next = true;
+                };
+            } catch (NumberFormatException nfe){
+                System.out.println("Not valid input, try again! (should look like '1')");
+            }
+        }
+        return userPosition;
+    }
+
+    public static Integer numberOfTests() {
+        int tests = 0;
+        while (tests == 0) {
+            Scanner inputContinuer = new Scanner(System.in);
+            String continuer = inputContinuer.nextLine();
+            try {
+                int integer = Integer.parseInt(continuer);
+                if (integer > 0){
+                    tests = integer;
+                };
+            } catch (NumberFormatException nfe){
+                System.out.println("Not valid input, try again! (must be integer bigger than 0 => should look like '9999')");
+            }
+        }
+        return tests;
+    }
+
+    public static void pressEnterToContinue(){
+        System.out.println("Press enter to continue....");
+        Scanner input = new Scanner(System.in);
+        String continuer = input.nextLine();
+        return;
+    }
+
+    public static int[] battle(Duelist duelist1, Duelist duelist2, int nmbrOfTests){
+        int counter1 = 0;
+        int counter2 = 0;
+        for (int i = 0; i < nmbrOfTests; i++) {
+            double result1 = ThreadLocalRandom.current().nextDouble(0, duelist1.getAccuracy());
+            double result2 = ThreadLocalRandom.current().nextDouble(0, duelist2.getAccuracy());
+            if (result1 > result2) {
+                counter1++;
+            } else {
+                counter2++;
+            }
+        }
+        int[] battleResults = {counter1, counter2};
+        return battleResults;
+    }
 
 }
